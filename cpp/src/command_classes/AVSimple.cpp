854c718ac5635d4b0bc1c5d1f4c3374fdd5cb703
anton.novgorodcev <anton.novgorodcev@gmail.com>
2018-07-26 21:17:17 +0300

    AVSimple.cpp improved;

--- cpp/src/command_classes/AVSimple.cpp
+++ cpp/src/command_classes/AVSimple.cpp
@@ -1,14 +1,16 @@
 #include "command_classes/CommandClasses.h"
 #include "command_classes/MultiInstance.h"
 #include "command_classes/AVSimple.h"
+#include "command_classes/AVSimpleCommandItem.h"
 #include "Defs.h"
 #include "Msg.h"
 #include "Node.h"
 #include "Driver.h"
 #include "platform/Log.h"
-#include <chrono>
+#include <vector>
 
 #include "value_classes/ValueShort.h"
+#include "value_classes/ValueList.h"
 
 using namespace OpenZWave;
 
@@ -42,32 +44,39 @@ bool AVSimple::SetValue
 	Value const& _value
 )
 {
+	uint16 shortval;
 	if (ValueID::ValueType_Short == _value.GetID().GetType())
 	{
 		ValueShort const* value = static_cast<ValueShort const*>(&_value);
-		uint16 shortval = value->GetValue();
-		uint8 instance = _value.GetID().GetInstance();
-		Msg* msg = new Msg("SimpleAVCmd_Set", GetNodeId(), REQUEST, FUNC_ID_ZW_SEND_DATA, true);
-		msg->SetInstance(this, instance);
-		msg->Append(GetNodeId());
-		msg->Append(8); // Length
-		msg->Append(GetCommandClassId());
-		msg->Append(SimpleAVCmd_Set);
-		msg->Append(m_sequence++); // Crutch
-		msg->Append(0);
-		msg->Append(0);
-		msg->Append(0);
-		msg->Append(shortval >> 8);
-		msg->Append(shortval & 0xff);
-		msg->Append(GetDriver()->GetTransmitOptions());
-		GetDriver()->SendMsg(msg, Driver::MsgQueue_Send);
-
-		if (m_sequence == 255)
-			m_sequence = 0;
-
-		return true;
+		shortval = value->GetValue();
 	}
-	return false;
+	else if (ValueID::ValueType_List == _value.GetID().GetType())
+	{
+		ValueList const* value = static_cast<ValueList const*>(&_value);
+		shortval = value->GetItem()->m_value;
+	}
+	else return false;
+
+	uint8 instance = _value.GetID().GetInstance();
+	Msg* msg = new Msg("SimpleAVCmd_Set", GetNodeId(), REQUEST, FUNC_ID_ZW_SEND_DATA, true);
+	msg->SetInstance(this, instance);
+	msg->Append(GetNodeId());
+	msg->Append(8); // Length
+	msg->Append(GetCommandClassId());
+	msg->Append(SimpleAVCmd_Set);
+	msg->Append(m_sequence++); // Crutch
+	msg->Append(0);
+	msg->Append(0);
+	msg->Append(0);
+	msg->Append(shortval >> 8);
+	msg->Append(shortval & 0xff);
+	msg->Append(GetDriver()->GetTransmitOptions());
+	GetDriver()->SendMsg(msg, Driver::MsgQueue_Send);
+
+	if (m_sequence == 255)
+		m_sequence = 0;
+
+	return true;
 }
 
 //-----------------------------------------------------------------------------
@@ -81,6 +90,52 @@ void AVSimple::CreateVars
 {
 	if (Node* node = GetNodeUnsafe())
 	{
-		node->CreateValueShort(ValueID::ValueGenre_User, GetCommandClassId(), 1, 0, "OutputMain", "", false, true, 0, 0);
+		// Create list value
+		vector<ValueList::Item> items;
+		vector<AVSimpleCommandItem> commands = AVSimpleCommandItem::GetCommands();
+		vector<AVSimpleCommandItem>::iterator iterator;
+		string helpList = "Possible values: \n";
+		string helpNumeric = "Possible values: \n";
+		for (iterator = commands.begin(); iterator != commands.end(); iterator++)
+		{
+			AVSimpleCommandItem command = *iterator;
+			if (command.GetVersion() <= GetVersion())
+			{
+				ValueList::Item item;
+				item.m_value = command.GetCode();
+				item.m_label = command.GetName();
+				items.push_back(item);
+
+				// ValueList - create command description for help
+				string codeStr = "ZWave command number: " + command.GetCode();
+				string descriptionFull = "";
+				if (command.GetDescription() == "")
+				{
+					descriptionFull = command.GetName() + " (" + codeStr + ")\n";
+				}
+				else
+				{
+					descriptionFull = command.GetName() + " (" + command.GetDescription() + "; " + codeStr + ")\n";
+				}
+				helpList +=  descriptionFull;
+
+				// ValueShort - create command description for help 
+				if (command.GetDescription() == "")
+				{
+					descriptionFull = command.GetCode() + " (" + command.GetName() + ")\n";
+				}
+				else
+				{
+					descriptionFull = command.GetCode() + " (" + command.GetName() + "; " + command.GetDescription() + ")\n";
+				}
+				helpNumeric += descriptionFull;
+			}
+		}
+
+		node->CreateValueList(ValueID::ValueGenre_User, GetCommandClassId(), _instance, 0, "OutputAVCommand_" + _instance, "", false, true, 2, items, 0, 0, helpList);
+		
+		// Create a similar numeric value
+		node->CreateValueShort(ValueID::ValueGenre_User, GetCommandClassId(), _instance, 0, "OutputAVCommandNumber_" + _instance, "", false, true, 0, 0, helpNumeric);
 	}
+
 }
